599baacdebc9d3daa4ca61af5c0803feefcdbcf0
Jay Gazula <jaygazula27@gmail.com>
2022-01-06 02:10:15 -0500

    Updated i3 config with polybar

---
+++ roles/polybar/templates/launchpolybar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+echo "-------------" | tee -a /tmp/polybarbottom.log /tmp/polybartop.log
+polybar bottom 2>&1 | tee -a /tmp/polybarbottom.log & disown
