8a2d35d1f7f0c5d1fd7eae10480c6c9ea1deba4b
Jay Gazula <jaygazula27@gmail.com>
2022-01-07 00:14:34 -0500

    Started working on top polybar

--- roles/polybar/templates/launchpolybar.sh
+++ roles/polybar/templates/launchpolybar.sh
@@ -8,4 +8,5 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 echo "-------------" | tee -a /tmp/polybarbottom.log /tmp/polybartop.log
+polybar top 2>&1 | tee -a /tmp/polybartop.log & disown
 polybar bottom 2>&1 | tee -a /tmp/polybarbottom.log & disown
